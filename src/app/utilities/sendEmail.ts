d330a3df752a954b4c176034a7f7c3b884d8f981
manik <maniksarker652@gmail.com>
2025-03-19 14:10:20 +0600

    make some changes in send email function

--- src/app/utilities/sendEmail.ts
+++ src/app/utilities/sendEmail.ts
@@ -1,29 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-// import nodemailer from 'nodemailer';
-// import config from '../config';
-// export const sendEmail = async (to: string, subject: string, html: string) => {
-//   console.log('receiver email', to);
-//   console.log( config.smtp.smtp_host)
-//   console.log( config.smtp.smtp_pass)
-//   const transporter = nodemailer.createTransport({
-//     host: 'smtp.gmail.com',
-//     port: 465,
-//     secure: config.NODE_ENV === 'production',
-//     auth: {
-//       //   TODO: replace `user` and `pass` values from <https://forwardemail.net>
-//       user: config.smtp.smtp_host,
-//       pass: config.smtp.smtp_pass,
-//     },
-//   });
-//   await transporter.sendMail({
-//     from: 'maniksarker265@gmail.com', // sender address
-//     to, // list of receivers
-//     subject: subject, // Subject line
-//     text: '', // plain text body
-//     html, // html body
-//   });
-// };
-
 import nodemailer from 'nodemailer';
 import config from '../config';
 
