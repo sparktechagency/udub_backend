2d548348c7b69b7584b46b252102c07ea05f525a
manik <maniksarker652@gmail.com>
2025-04-09 15:04:07 +0600

    fixed some issues

--- src/app/middlewares/rateLimit.ts
+++ src/app/middlewares/rateLimit.ts
@@ -3,20 +3,20 @@ import Redis, { RedisKey } from 'ioredis';
 
 const redis = new Redis();
 
-// Middleware function for rate limiting
+// Middleware function for rate limiting-----------------
 const rateLimit = async (req: Request, res: Response, next: NextFunction) => {
   const ip = req.ip;
   const limit = 10;
   const windowTime = 60;
 
-  // Increment request count
+  // Increment request count-------------------
   const requests = await redis.incr(ip as RedisKey);
 
-  // Set expiry time for the first request
+  // Set expiry time for the first request------------------
   if (requests === 1) {
     await redis.expire(ip as RedisKey, windowTime);
   }
-  // If limit exceeded, return error
+  // If limit exceeded, return error----------------
   if (requests > limit) {
     return res
       .status(429)
