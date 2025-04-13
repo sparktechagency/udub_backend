1539d3953256c3b99b50882c8d58bdbcf2a6f068
manik <maniksarker652@gmail.com>
2025-04-13 12:29:32 +0600

    s

--- src/server.ts
+++ src/server.ts
@@ -30,7 +30,7 @@ async function main() {
 
     initializeSocket(myServer);
 
-    // Global unhandled rejection handler
+    // Global unhandled rejection handler---------
     process.on('unhandledRejection', (error) => {
       logger.error('Unhandled Rejection:', error);
       if (myServer) {
