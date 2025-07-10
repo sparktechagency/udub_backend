78a9a8daa1428d9c25fd83b870b4968884ec71e3
manik <maniksarker652@gmail.com>
2025-07-10 14:33:50 +0600

    setup roues for podcast

--- src/app/modules/podcast/podcast.routes.ts
+++ src/app/modules/podcast/podcast.routes.ts
@@ -1,25 +1,32 @@
-import express from "express";
-import auth from "../../middlewares/auth";
-import { USER_ROLE } from "../user/user.constant";
-import validateRequest from "../../middlewares/validateRequest";
-import podcastValidations from "./podcast.validation";
-import podcastController from "./podcast.controller";
-import { uploadFile } from "../../helper/fileUploader";
+import express from 'express';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
+import validateRequest from '../../middlewares/validateRequest';
+import podcastValidation from './podcast.validation';
+import podcastController from './podcast.controller';
 
 const router = express.Router();
 
+router.post(
+  '/create-podcast',
+  auth(USER_ROLE.superAdmin),
+  validateRequest(podcastValidation.createPodcastValidationSchema),
+  podcastController.createPodcast,
+);
+
 router.patch(
-    "/update-profile",
-    auth(USER_ROLE.user),
-    uploadFile(),
-    (req, res, next) => {
-        if (req.body.data) {
-            req.body = JSON.parse(req.body.data);
-        }
-        next();
-    },
-    validateRequest(podcastValidations.updatePodcastData),
-    podcastController.updateUserProfile
+  '/update-podcast/:id',
+  auth(USER_ROLE.superAdmin),
+  validateRequest(podcastValidation.updatePodcastValidationSchema),
+  podcastController.updatePodcast,
+);
+
+router.get('/all-podcasts', podcastController.getAllPodcasts);
+router.get('/get-single-podcast/:id', podcastController.getSinglePodcast);
+router.delete(
+  '/delete-podcast/:id',
+  auth(USER_ROLE.superAdmin),
+  podcastController.deletePodcast,
 );
 
 export const podcastRoutes = router;
